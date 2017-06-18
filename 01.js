93c42895ad4fcf50e82a754939fb4ac8ec9e85f4
ybfdrj <noreply>
2017-06-18 02:45:54 +0000

    Add module 01

---
+++ 01.js
@@ -0,0 +1,15 @@
+/**
+ * @module //01
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class 01
+ * @extends Montage
+ */
+exports.01 = Montage.specialize(/** @lends 01# */ {
+    constructor: {
+        value: function 01() {
+            this.super();
+        }
+    }
+});
